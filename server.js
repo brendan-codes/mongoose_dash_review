965cae8710c3a6fc326333b5d96d49e709f0e6ba
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-13 13:57:35 -0700

    added my code

--- server.js
+++ server.js
@@ -1,111 +1,99 @@
+var   express     = require('express'),
+      bodyParser  = require('body-parser'),
+      path        = require('path'),
+      io          = require('socket.io'),
+      mongoose    = require('mongoose')
+      app         = express();
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-var path = require('path');
-app.use(express.static(path.join(__dirname, './static')));
-app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
 
+//Routes
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/giraffes');
-var GiraffeSchema = new mongoose.Schema({
-	name: String,
-	age: Number
-}, {timestamps: true});
-
-mongoose.model('Giraffe', GiraffeSchema); 
-var Giraffe = mongoose.model('Giraffe'); 
-
-
-
-// Routes
-// Shows all giraffes
 app.get('/', function(req, res) {
-	Giraffe.find({}, function(err, giraffes) {
-		if(err) {
-			console.log('could not find giraffes');
-		} else {
-			console.log('successfully found giraffes');
-			console.log(giraffes);
-			var all_giraffes = giraffes;
-
-		}
-		res.render('allgiraffes', {all_giraffes: all_giraffes});
-	});	
-});
-
-// Shows form for creating a new giraffe
-app.get('/giraffes/new', function(req,res){
-	res.render('newgiraffe');
+  Animal.find({}, function(err, animals) {
+    if(err) {
+      console.log('something went wrong');
+    } else {
+        res.render('index', {animals: animals});
+    }
+  })
 })
 
-// Creates a new giraffe
-app.post('/giraffes', function(req,res){
-	console.log('POST DATA', req.body);
-	var giraffe = new Giraffe({name: req.body.name, age: req.body.age});
-	giraffe.save(function(err){
-		if (err){
-			console.log("Something went wrong");
-		} else {
-			console.log("Successfully added giraffe");
-			res.redirect('/');
-		}
-	})
+app.get('/species/new', function(req, res) {
+  res.render('create');
 })
 
-// Shows one giraffe
-app.get('/giraffes/:id', function(req,res){
-	Giraffe.findOne({_id: req.params.id}, function(err, giraffe) {
-		if (err){
-			console.log("Something went wrong");
-		} else {
-			console.log(giraffe);
-			res.render('onegiraffe', { giraffe: giraffe});
-		}	
-	})
+app.post('/species', function(req, res) {
+  var animal = new Animal({
+    Animal: req.body.Animal
+  });
+  animal.save(function(err) {
+    if(err) {
+      console.log('something went wrong');
+    } else {
+      console.log('successfully added animal');
+      res.redirect('/');
+    }
+  })
 })
 
-// Allows one giraffe to be edited
-app.get('/giraffes/:id/edit', function(req,res){
-	Giraffe.findOne({_id: req.params.id}, function(err, giraffe) {
-		if (err){
-			console.log("Something went wrong");
-		} else {
-			console.log(giraffe);
-			res.render('editgiraffe', {giraffe: giraffe});
-		}	
-	})
+app.get('/species/:id', function(req, res) {
+  Animal.findOne({_id: req.param.id}, function(err, animals) {
+    if(err) {
+      console.log('something went wrong');
+    } else {
+      console.log(animals);
+      res.render('show', {animals: req.body.Animal});
+    }
+  })
 })
 
-
-// Edits one giraffe's details
-app.post('/giraffes/:id', function(req,res){
-	Giraffe.update({_id: req.params.id}, {name: req.body.name, age: req.body.age}, function(err) {
-		if (err){
-			console.log("Something went wrong");
-		} else {
-			console.log("Successfully updated giraffe");
-			res.redirect('/');
-		}
-	})
+app.get('/species/:id/edit', function(req, res){
+  //need to pass the animal object
+  Animal.findOne({_id: req.params.id}, function(err, animals) {
+    if(err) {
+      console.log('something went wrong');
+    } else {
+      console.log('line56');
+      console.log(req.params.id);
+      console.log(animals);
+      res.render('edit', {animal: animals});
+    }
+  })
 })
 
-// Deletes giraffe based on ID
-app.post('/giraffes/:id/destroy', function(req,res){
-	Giraffe.remove({_id: req.params.id}, function(err){
-		if (err){
-			console.log("Something went wrong");
-		} else {
-			console.log("Successfully removed giraffe");
-			res.redirect('/');
-		}
-	})
+app.post('/species/:id', function(req, res){
+  //query that stores the updated information for that instance
+  Animal.update({_id: req.params.id}, {$set:
+    {Animal: req.body.Animal}
+  }, function(err, result){
+    res.redirect('/');
+  })
 })
+
+app.post('/species/:id/destroy', function(req, res){
+  Animal.remove({_id: req.params.id}, function(err, result){
+    if(err) {
+      console.log(err);
+    }
+    res.redirect('/');
+  })
+})
+
+//Mongoose
+mongoose.connect('mongodb://localhost/dashboard');
+
+var AnimalSchema = new mongoose.Schema({
+  Animal: {type: String}
+}, {timestamps: true})
+
+mongoose.model('Animal', AnimalSchema);
+
+var Animal = mongoose.model('Animal');
 
 app.listen(8000, function() {
-	console.log("Listening on port 8000");
-});
+  console.log("listening on port 8000");
+})
